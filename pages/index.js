2700d5742fdf69f5dcad657785a1b3181d7eecba
Raphael Quinones <wapquinones28@gmail.com>
2023-03-29 15:32:44 +0800

    changed UI text

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,9 @@ import facebookLogo from '../assets/facebook.png';
 import githubLogo from '../assets/github.png';
 import linkedinLogo from '../assets/linkedin.png';
 import {useState} from 'react';
-import Particles from "react-tsparticles";
 import { useCallback } from "react";
-import { loadFull } from "tsparticles";
 import { Analytics } from '@vercel/analytics/react';
 
-import options from "./particles.json"
 
 
 
@@ -104,28 +101,18 @@ const Home = () => {
   function onUserChangedText(event){
       setUserInput(event.target.value)
   }
-  const particlesInit = useCallback(async engine => {
-    console.log(engine);
-    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(engine);
-  }, []);
-  const particlesLoaded = useCallback(async container => {
-    await console.log(container);
-  }, []);
   return (
     <div className="root">
       <Head>
-        <title>Dream Interpreter</title>
+        <title>Pinoy Pocket Lawyer</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>dreams. what do they mean?</h1>
+            <h1>Pinoy Pocket Lawyer</h1>
           </div>
           <div className="header-subtitle">
-            <h2>Tell me about a very specific dream that you had. The more specific, the better. I'll tell you what it means.</h2>
+            <h2>Worried about yourself? Ask anything here</h2>
           </div>
         </div>
       </div>
@@ -201,13 +188,6 @@ const Home = () => {
           <Image src={githubLogo} width = {17} height = {17}/>
         </a>
       </div>
-      
-      <Particles
-      id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
-        options={options}
-      />
       <Analytics />
     </div>
     
