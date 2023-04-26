fd5163156b6ce433e1f34b56c8ea7b14859fb429
Raphael Quinones <wapquinones28@gmail.com>
2023-04-26 23:21:01 +0800

    updated prompt

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -8,7 +8,7 @@ const openai = new OpenAIApi(configuration);
 //Interpret the dream that I will type in the next line:
 //Tell me some funny believable false facts about the topic I will type
 
-const basePromptPrefix = "Gather everything you can about Philippine Laws. Every single detail, and then answer the question in the next line with practical solutions and how to overcome that particular legal problem in the Philippines";
+const basePromptPrefix = "Gather everything you can about Philippine Laws. Every single detail, and then answer the question specifically in the next line with practical solutions and how to overcome that particular legal problem in the Philippines. Make the answer as detailed as possible. Include the law's name, number, etc. Answer in Tagalog";
 const generateAction = async (req, res) => {
     //Run first prompt
     console.log(`API: ${basePromptPrefix}${req.body.userInput}\n`)
