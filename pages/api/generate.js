51045cc5c6561ecdc94051bb9e74cb494902830b
Raphael Quinones <wapquinones28@gmail.com>
2023-04-26 23:32:32 +0800

    updated to gpt 3.5 turbo

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -8,19 +8,20 @@ const openai = new OpenAIApi(configuration);
 //Interpret the dream that I will type in the next line:
 //Tell me some funny believable false facts about the topic I will type
 
-const basePromptPrefix = "Gather everything you can about Philippine Laws. Every single detail, and then answer the question specifically in the next line with practical solutions and how to overcome that particular legal problem in the Philippines. Make the answer as detailed as possible. Include the law's name, number, etc. Answer in Tagalog";
+const basePromptPrefix = "Gather everything you can about Philippine Laws. Every single detail, and then answer the question specifically in the next line with practical solutions and how to overcome that particular legal problem in the Philippines. Make the answer as detailed as possible. Include the law's name, number, etc. Answer in Tagalog. ";
 const generateAction = async (req, res) => {
     //Run first prompt
     console.log(`API: ${basePromptPrefix}${req.body.userInput}\n`)
 
-    const baseCompletion = await openai.createCompletion({
-        model: 'text-davinci-003',
-        prompt: `${basePromptPrefix}${req.body.userInput}`,
-        temperature: 0.9,
-        max_tokens: 250
+    const baseCompletion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [{role: "user", content: `${basePromptPrefix}${req.body.userInput}` }],
+        temperature: 0.7,
+        max_tokens: 400
     });
 
-    const basePromptOutput = baseCompletion.data.choices.pop();
+    const basePromptOutput = baseCompletion.data.choices[0].message.content;
+    console.log(basePromptOutput)
 
     res.status(200).json({ output: basePromptOutput});
 };
